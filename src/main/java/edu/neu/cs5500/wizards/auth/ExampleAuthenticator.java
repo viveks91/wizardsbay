ed26d185825fd93502b44039e82b504208301f52
Amala <amalajoshy@gmail.com>
2016-07-08 14:06:50 -0700

    Minor changes

--- src/main/java/edu/neu/cs5500/wizards/auth/ExampleAuthenticator.java
+++ src/main/java/edu/neu/cs5500/wizards/auth/ExampleAuthenticator.java
@@ -1,4 +1,3 @@
-
 package edu.neu.cs5500.wizards.auth;
 
 import com.google.common.base.Optional;
@@ -11,15 +10,15 @@ import io.dropwizard.auth.basic.BasicCredentials;
 public class ExampleAuthenticator implements Authenticator<BasicCredentials, User> {
 
     private final UserDAO userDao;
-    
-    public ExampleAuthenticator(UserDAO userDao) {
-        this.userDao = userDao;
+
+    public ExampleAuthenticator(UserDAO userDao){
+        this.userDao =userDao;
     }
 
     @Override
     public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
         User user = userDao.retrieve(credentials.getUsername());
-        if (user != null && user.getPassword().equals(credentials.getPassword())) {
+        if(user != null && user.getPassword().equals(credentials.getPassword())){
             return Optional.of(user);
         }
         return Optional.absent();
