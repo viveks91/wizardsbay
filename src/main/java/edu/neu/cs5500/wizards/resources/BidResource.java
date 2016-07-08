1c71b3a2878c31d9992cbebadbd4e92cf803e26e
susanna9 <edens.s@husky.neu.edu>
2016-07-08 13:33:14 -0700

    Bid Resource edits/documentation

--- src/main/java/edu/neu/cs5500/wizards/resources/BidResource.java
+++ src/main/java/edu/neu/cs5500/wizards/resources/BidResource.java
@@ -26,8 +26,13 @@ public class BidResource {
         this.bidDao = bidDao;
     }
 
-    //Create bid only if given bid is higher than the current bid on the item
-    // OR if it is the first bid on an item
+    /**
+     * Creates a bid and returns it only if the given bid's bid amount is greater than the current highest bid for
+     * the item or if it is the first bid on an item.
+     *
+     * @param bid the bid to create and return
+     * @return the new bid.
+     */
     @POST
     @Timed
     @UnitOfWork
@@ -51,7 +56,13 @@ public class BidResource {
         return newBid;
     }
 
-    //get all bids for a given item
+
+    /**
+     * Retrieves the history of bids for an item given the item's id.
+     *
+     * @param itemId the id of the item
+     * @return all bids for a specific item
+     */
     @GET
     @Path("/history/{itemId}")
     @Timed
@@ -62,7 +73,12 @@ public class BidResource {
     }
 
 
-    //get bid by id
+    /**
+     * Retrieve a bid given the bid's id.
+     *
+     * @param id the id of the bid
+     * @return the bid with the id matching the given id
+     */
     @GET
     @Path("/{id}")
     @Timed
@@ -73,19 +89,32 @@ public class BidResource {
     }
 
 
-    //get highest bid for an item given itemId
+    /**
+     * Retrieve the highest current bid for an item given the item's id.
+     *
+     * @param itemId
+     * @return
+     */
     @GET
     @Path("/item/{itemId}")
     @Timed
     @UnitOfWork
     @ExceptionMetered
-    public Bid getByItemId(@PathParam("itemId") int itemId) {
+    public Bid getHighestBid(@PathParam("itemId") int itemId) {
         Bid highest = bidDao.retrieveHighestBid(itemId);
+        if (highest == null) {
+            ResponseException.formatAndThrow(Response.Status.BAD_REQUEST, "There are no bids for this item yet.");
+        }
         return highest;
     }
 
 
-    //Delete a bid by bid id
+    /**
+     * Given the id of a bid, delete the bid with the matching id from the database.
+     *
+     * @param bidId the id of the bid
+     * @return an string representing an empty set
+     */
     @DELETE
     @Path("/{bidId}")
     @Timed
