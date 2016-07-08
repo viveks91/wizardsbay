2eb9a529fdc161b1a6c58f6193963659e657051d
susanna9 <edens.s@husky.neu.edu>
2016-07-08 13:44:00 -0700

    Feedback Resource edits/documentation & exceptions

--- src/main/java/edu/neu/cs5500/wizards/resources/FeedbackResource.java
+++ src/main/java/edu/neu/cs5500/wizards/resources/FeedbackResource.java
@@ -4,10 +4,12 @@ import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
 import edu.neu.cs5500.wizards.core.Feedback;
 import edu.neu.cs5500.wizards.db.FeedbackDAO;
+import edu.neu.cs5500.wizards.exception.ResponseException;
 import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
@@ -16,7 +18,6 @@ import java.util.List;
 @Path("/feedback")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-//@Api(value = "/feedback", description = "Feedback")
 public class FeedbackResource {
 
     private final FeedbackDAO feedbackDao;
@@ -25,7 +26,12 @@ public class FeedbackResource {
         this.feedbackDao = feedbackDao;
     }
 
-    //Create feedback
+    /**
+     * Creates a feedback for a user.
+     *
+     * @param feedback the feedback to create
+     * @return the created feedback
+     */
     @POST
     @Timed
     @UnitOfWork
@@ -36,7 +42,14 @@ public class FeedbackResource {
         return newfeedback;
     }
 
-    //get all feedback for a given user
+
+    /**
+     * Given an id for a user, retrieve the list of feedback for that user. If the user does not exist, no feedback
+     * will be returned.
+     *
+     * @param userid the id of the user
+     * @return a list of feedback for a given user
+     */
     @GET
     @Path("/user/{userid}")
     @Timed
@@ -46,29 +59,44 @@ public class FeedbackResource {
         return feedbackDao.retrieve(userid);
     }
 
-    //get feedback by id
+    /**
+     * Given an id, retrieve feedback with the matching id. If there is no such feedback in the database, throw
+     * an exception.
+     *
+     * @param id the id of the feedback
+     * @return the feedback matching the given id
+     */
     @GET
     @Path("/{id}")
     @Timed
     @UnitOfWork
     @ExceptionMetered
     public Feedback getOne(@PathParam("id") int id) {
-        return feedbackDao.retrieveOne(id);
+        Feedback feedback = feedbackDao.retrieveOne(id);
+        if (feedback == null) {
+            ResponseException.formatAndThrow(Response.Status.BAD_REQUEST, "No feedback matches your request");
+        }
+        return feedback;
     }
 
+    /**
+     * Given a feedback, delete that feedback from the database. If the feedback is not found, throw an exception.
+     *
+     * @param existingfeedback the feedback to delete
+     * @return a string representing an empty set
+     */
     @DELETE
     @Timed
     @UnitOfWork
     @ExceptionMetered
-    /* delete feedback given feedback*/
     public String delete(Feedback existingfeedback) {
-
-        feedbackDao.delete(existingfeedback);
-        //> Why return a string?
+        Feedback feedback = feedbackDao.retrieveOne(existingfeedback.getId());
+        if (feedback == null) {
+            ResponseException.formatAndThrow(Response.Status.BAD_REQUEST, "Feedback not found");
+        }
+        feedbackDao.delete(feedback);
         return "{}";
     }
-
-
 
 
 }
