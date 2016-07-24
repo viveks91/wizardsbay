60be28860f1a1e2f1f2779c64a653cd7db1cd907
Amala <amalajoshy@gmail.com>
2016-07-24 16:17:20 -0700

    Updating Mailgun credentials/keys

--- src/main/java/edu/neu/cs5500/wizards/mail/MailNotification.java
+++ src/main/java/edu/neu/cs5500/wizards/mail/MailNotification.java
@@ -8,9 +8,9 @@ import net.sargue.mailgun.content.Body;
  * Created by amala on 24/07/16.
  */
 public class MailNotification {
-    private static final String MAILGUN_DOMAIN = "app8bfbc96f0e2142968e87558a2d0671db.mailgun.org";
+    private static final String MAILGUN_DOMAIN = "wizardsbay.tixter.us";
     private static final String MAILGUN_API_KEY = "key-e9832d2be5768b87bd6cc56fad6ab3e5";
-    private static final String MAILGUN_LOGIN = "wizardsbay-noreply@app8bfbc96f0e2142968e87558a2d0671db.mailgun.org";
+    private static final String MAILGUN_LOGIN = "no-reply@wizardsbay.tixter.us";
 
     private static Configuration configuration = new Configuration()
             .domain(MAILGUN_DOMAIN)
